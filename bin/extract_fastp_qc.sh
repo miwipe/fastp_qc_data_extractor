47d17f1ba8144534665ed6cdbe00d266027be7b2
Mikkel Winther Pedersen <mwpedersen@snm.ku.dk>
2026-01-28 16:33:29 +0100

    Update header for fastp QC output format

--- bin/extract_fastp_qc.sh
+++ bin/extract_fastp_qc.sh
@@ -80,7 +80,7 @@ normalize_count() {
 }
 
 # --- header ---
-printf "library_id\tsequencing_mode\tduplication_rate_pct\ttotal_reads_before_filtering\ttotal_reads_after_filtering\treads_too_short_pct\tlow_complexity_pct\tlow_quality_pct\tgc_content\tinsert_size_peak\n"
+printf "library_id\tfastp_sequencing_mode\tfastp_duplication_rate_pct\tfastp_total_reads_before_filtering\tfastp_total_reads_after_filtering\tfastp_reads_too_short_pct\tfastp_low_complexity_pct\tfastp_low_quality_pct\tfastp_gc_content\tfastp_insert_size_peak\n"
 
 # --- extract per file ---
 for f in "${files[@]}"; do
